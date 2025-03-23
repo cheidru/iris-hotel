6bbe90e00ed13b58678dd57353dafa631820b026
Igor Checheurov <chei@yandex.ru>
2025-03-23 15:51:54 +0300

    fix: fix base address in vite.config.ts and basename in main.tsx for gh-pages deploy

--- pilot-villa-react/src/main.tsx
+++ pilot-villa-react/src/main.tsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={import.meta.env.NODE_ENV === 'production' ? '/iris-hotel' : '/'}>
+  // https://ru.stackoverflow.com/questions/1539856/%D0%91%D0%B5%D0%BB%D1%8B%D0%B9-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD-%D0%BF%D1%80%D0%B8-deploy-vite-react-%D0%BD%D0%B0-gh-pages
+  // <BrowserRouter basename={import.meta.env.NODE_ENV === 'production' ? '/iris-hotel' : '/'}>
+  <BrowserRouter basename='/iris-hotel'>
     <App />
   </BrowserRouter>
 )
--- pilot-villa-react/vite.config.ts
+++ pilot-villa-react/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(
   {
-    base: import.meta.env?.BASE_URL === 'production' ? '/iris-hotel' : '/',
+    // base: import.meta.env?.BASE_URL === 'production' ? '/iris-hotel' : '/',
+    base: '/iris-hotel',
     plugins: [react()],
   }
 
