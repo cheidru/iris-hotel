55e49b76b33e87cf7191fd73af1ed1f784decf71
Igor Checheurov <chei@yandex.ru>
2025-04-14 00:02:47 +0300

    feat: add cottage pictures

--- pilot-villa-react/src/services/data.ts
+++ pilot-villa-react/src/services/data.ts
@@ -2,7 +2,7 @@ import { CardProps } from "./interface"
 
 export const cottageSpain : CardProps = {
   title: "1. Коттедж «Испанский дворик»",
-  src: new URL('/src/assets/img/cottage/japan/jap1.jpg', import.meta.url).href,
+  src: new URL('/src/assets/img/cottage/spain/spain1.jpg', import.meta.url).href,
   descr: `Коттедж «Испанский дворик» для семьи до 4-х человек. Двухэтажный коттедж состоит из двух номеров. 
   Количество спальных мест - четыре. Площадь одного номера - 30 кв.м. В каждом номере одна комната. 
   Санузел и душ есть в каждом номере. В каждом номере есть двуспальная кровать, стулья, шкаф, прикроватные тумбочки, вешалка, зеркало,
@@ -10,55 +10,55 @@ export const cottageSpain : CardProps = {
   Коттедж имеет кухню и зону отдыха - веранда, мангал, шезлонги.`,
   cards: [
     {
-      src: new URL('/src/assets/img/cottage/japan/jap2.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain2.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap3.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain3.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap4.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain4.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap5.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain5.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap7.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain7.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap8.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain8.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap9.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain9.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap10.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain10.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap11.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain11.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap12.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain12.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap14.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain14.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap15.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain15.jpg', import.meta.url).href,
       descr: "Описание",
     },
     {
-      src: new URL('/src/assets/img/cottage/japan/jap16.jpg', import.meta.url).href,
+      src: new URL('/src/assets/img/cottage/spain/spain16.jpg', import.meta.url).href,
       descr: "Описание",
     },
   ],
@@ -101,6 +101,14 @@ export const cottageBamboo : CardProps = {
       src: new URL('/src/assets/img/cottage/bamboo/bamboo6.jpg', import.meta.url).href,
       descr: "Описание",
     },
+    {
+      src: new URL('/src/assets/img/cottage/bamboo/bamboo7.jpg', import.meta.url).href,
+      descr: "Описание",
+    },
+    {
+      src: new URL('/src/assets/img/cottage/bamboo/bamboo8.jpg', import.meta.url).href,
+      descr: "Описание",
+    },
   ],
   video: {
     src: "https://vkvideo.ru/video_ext.php?oid=-197335368&id=456239042&hd=1",
@@ -139,7 +147,11 @@ export const cottageWood : CardProps = {
     {
       src: new URL('/src/assets/img/cottage/wood/wood6.jpg', import.meta.url).href,
       descr: "Описание",
-    },    
+    },
+    {
+      src: new URL('/src/assets/img/cottage/wood/wood7.jpg', import.meta.url).href,
+      descr: "Описание",
+    }, 
   ],
   video: {
     //<iframe src="https://vkvideo.ru/video_ext.php?oid=-197335368&id=456239043&hd=1&autoplay=1" width="640" height="360" allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;" frameborder="0" allowfullscreen></iframe>
