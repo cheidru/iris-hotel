a4db00e34aacad33e0ef37b35f4f24304f5f7a0b
Igor Checheurov <chei@yandex.ru>
2025-05-29 13:14:53 +0300

    feat: fix last updates for workplace change

--- pilot-villa-react/src/pages/Details.tsx
+++ pilot-villa-react/src/pages/Details.tsx
@@ -1,9 +1,12 @@
 import { useParams } from "react-router-dom"
+import MainCard from "../components/MainCard";
 
 export default function Details() {
   const param = useParams();
   return (
     <main>
+      <MainCard
+       />
       <h1>Detailed description about {param.id}</h1>
     </main>
   )
